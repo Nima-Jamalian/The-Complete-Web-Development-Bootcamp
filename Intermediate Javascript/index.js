0a4105460c60bf279412e5dadc4073c8034d6ff7
Nima Jamalian <nima.jamalian80@gmail.com>
2021-09-29 20:47:53 +0100

    Create index.js

---
+++ Intermediate Javascript/index.js
@@ -0,0 +1,55 @@
+//Math.random gives number within the range 0 - 0.999999999999999999999 (16 decimal places)
+// var n = Math.random();
+// n = n * 6;
+// n = Math.floor(n) + 1;
+// console.log(n);
+
+//---------------------------------------------------------------
+//Random love generator challange 
+// var name1 = window.prompt("Enter name of frist person");
+// var name2 = window.prompt("Enter name of secound person");
+// var loveNumber = Math.random();
+// loveNumber = loveNumber *100;
+// loveNumber = Math.floor(loveNumber) +1;
+// if(loveNumber > 70){
+//     alert("Your love score is so high!");
+// } else {
+//     alert("Your love score is " + loveNumber + "%");
+// }
+
+
+//---------------------------------------------------------------
+//Comparators abd Equality
+//=== check if the data type also match
+//== does  not care about data type
+
+// var loveNumber = Math.random();
+// loveNumber = loveNumber *100;
+// loveNumber = Math.floor(loveNumber) +1;
+// if(loveNumber > 70){
+//     alert("Your love score is " + loveNumber + "%" + " You love each other like Kanye loves Kanye." );
+// } 
+// if(loveNumber > 30 && loveNumber <= 70){
+//     alert("Your love score is " + loveNumber + "%");
+// } 
+
+// if(loveNumber <= 30){
+//     alert("Your love score is " + loveNumber + "%" + " You go together like oil and water.");
+// }
+//---------------------------------------------------------------
+
+//BMW Calculator Advanced
+// BMI Calculator Advanced (IF/ELSE)
+// Previously, we've created a function that is able to calculate the BMI. But once we get a result, we
+// will want to tell the user what the number means.
+// Write a function that outputs (returns) a different message depending on the BMI.
+// BMI <18.5, the output should be: "Your BMl is <bmis, so you are underweight."
+// BMI 18.5-24.9, the output should be "Your BMI is <bmis, so you have a normal weight."
+// BMI >24.9, the output should be "Your BMI is <bmis, so you are overweight."
+// The message MUST be returned as an output from your function. You should NOT NEED to use
+// alert, prompt or console.log in this challenge.
+// IMPORTANT the message wording has to match precisely for the code to pass the validation.
+// Including punctuation and capitalisation.
+
+
+//---------------------------------------------------------------
