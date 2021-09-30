8ddb4faa8afb48ac3533e9afe49ba78649052ad0
Nima Jamalian <nima.jamalian80@gmail.com>
2021-09-30 23:23:41 +0100

    End of Section 9 DOM

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,21 @@
-document.querySelector("h1").innerHTML="Good Bye";
+document.querySelector(".btn").style.backgroundColor = "red";//bad practice we should not change the CSS directly in JavaScript
+document.querySelector("#title").classList.toggle("huge");
+
+
+/*
+innerHTML -> Returns everything inside the selected element -> it is useful so you can also change the style of text like strong or em(italicised)
+textContent -> Returns only the text
+document. querySelector ("h1" ). innerHTML;
+"‹strong>Hello</strong>"
+
+document. querySelector("h1"). textContent;
+"Hello"
+
+document. querySelector ("h1" ). innerHTML = "<em>Good Bye</em>"
+"<em>Good Bye</em>";
+*/
+document.querySelector("#title").innerHTML = "<em> Good Bye! </em>";
+
+
+
+document.querySelector("a").attributes.href.value = "https://www.bing.com/";
